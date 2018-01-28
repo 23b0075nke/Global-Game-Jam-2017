dc1d16884bb2644ca3a04796c932cf03a1f01fac
Taro Omiya <taro.omiya@omiyagames.com>
2018-01-27 23:03:33 -0500

    Adding intial background stuff.  Need placeholder artwork.

---
+++ Assets/Project/Scripts/StarfieldBackgroundGenerator.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarfieldBackgroundGenerator : MonoBehaviour
+{
+    [System.Serializable]
+    public class GeneratorModel
+    {
+        [SerializeField]
+        private int maxNumStars;
+        [SerializeField]
+        private Transform min3dRange;
+        [SerializeField]
+        private Transform max3dRange;
+        private readonly HashSet<StarfieldObject> starfieldObjects = new HashSet<StarfieldObject>();
+
+        public int MaxNumStars
+        {
+            get
+            {
+                return maxNumStars;
+            }
+        }
+
+        public Vector3 RandomPosition
+        {
+            get
+            {
+                return new Vector3(
+                    Random.Range(Min3dRange.x, Max3dRange.x)
+                    , Random.Range(Min3dRange.y, Max3dRange.y)
+                    , Random.Range(Min3dRange.z, Max3dRange.z)
+                    );
+            }
+        }
+
+        public HashSet<StarfieldObject> StarfieldObjects
+        {
+            get
+            {
+                return starfieldObjects;
+            }
+        }
+
+        public Vector3 Min3dRange
+        {
+            get
+            {
+                return min3dRange.position;
+            }
+        }
+
+        public Vector3 Max3dRange
+        {
+            get
+            {
+                return max3dRange.position;
+            }
+        }
+    }
+
+    [SerializeField]
+    private float maxDelay;
+    [SerializeField]
+    private StarfieldObject[] allStars;
+
+    [Header("Range")]
+    [SerializeField]
+    private GeneratorModel background;
+    [SerializeField]
+    private GeneratorModel foreground;
+
+    public float NextDelay
+    {
+        get
+        {
+            return Random.value * maxDelay;
+        }
+    }
+
+    public Vector3 GetRandomStarPosition(StarfieldObject star)
+    {
+        if(foreground.StarfieldObjects.Contains(star) == true)
+        {
+            return foreground.RandomPosition;
+        }
+        else
+        {
+            return background.RandomPosition;
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        GenerateStars(background);
+    }
+
+    private void GenerateStars(GeneratorModel model)
+    {
+        int randomIndex;
+        GameObject clone;
+        StarfieldObject clonedScript;
+        for (int index = 0; index < model.MaxNumStars; ++index)
+        {
+            // Generate a random star
+            randomIndex = Random.Range(0, allStars.Length);
+            clone = Instantiate<GameObject>(allStars[randomIndex].gameObject);
+
+            // Setup the script
+            clonedScript = clone.GetComponent<StarfieldObject>();
+            model.StarfieldObjects.Add(clonedScript);
+            clonedScript.Setup(this);
+        }
+    }
+}
---
+++ Assets/Project/Scripts/StarfieldObject.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class StarfieldObject : MonoBehaviour
+{
+    [SerializeField]
+    private string triggerName = "play";
+    [SerializeField]
+    private Vector3 rotateRange = Vector3.right;
+
+    private Animator animator = null;
+    private StarfieldBackgroundGenerator generator;
+
+    public Animator Animator
+    {
+        get
+        {
+            if(animator == null)
+            {
+                animator = GetComponent<Animator>();
+            }
+            return animator;
+        }
+    }
+
+    public void Setup(StarfieldBackgroundGenerator generator)
+    {
+        this.generator = generator;
+        NextLocation();
+    }
+
+    public void NextLocation()
+    {
+        if(generator != null)
+        {
+            StartCoroutine(DelayAnimate(generator.NextDelay));
+        }
+    }
+
+    IEnumerator DelayAnimate(float delay)
+    {
+        // Position this star at a new location
+        transform.position = generator.GetRandomStarPosition(this);
+        transform.Rotate(rotateRange * Random.value);
+
+        // Delay the time
+        yield return new WaitForSeconds(delay);
+
+        // Animate
+        Animator.SetTrigger(triggerName);
+    }
+}
