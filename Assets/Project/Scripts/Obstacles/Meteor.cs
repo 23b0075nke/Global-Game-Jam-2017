c77cafb2246c0b2bc4c1304c6b53b3261642e772
R King <rking487@gmail.com>
2018-01-27 21:06:04 -0500

    Parallax scrolling background branch merge

---
+++ Assets/Project/Scripts/Obstacles/Meteor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Obstacle
+{
+	/**
+	* Defines behavior for meteor obstacle objects
+	*/ 
+	public class Meteor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 pos = transform.position;
+		transform.position = new Vector3( pos.x, pos.y, pos.z );
+	}
+}
+}
