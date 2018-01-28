45ff0990be346b1b2608d05e82887b7140b7d581
twainwright <twainwright42@gmail.com>
2018-01-28 11:25:01 -0500

    New SFX scene added so I don't mess other people up making SFX prefabs

--- Assets/Project/Scripts/Audio/SFXController.cs
+++ Assets/Project/Scripts/Audio/SFXController.cs
@@ -7,27 +7,46 @@ public class SFXController : MonoBehaviour {
     [SerializeField]
     private AudioSource ActivatedNodePlayer;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    //Define Audio Clips
+    AudioClip Node_Turn_On_LowC;
+    AudioClip Node_Turn_On_D;
+    AudioClip Node_Turn_On_E;
+    AudioClip Node_Turn_On_F;
+    AudioClip Node_Turn_On_G;
+    AudioClip Node_Turn_On_A;
+    AudioClip Node_Turn_On_B;
+    AudioClip Node_Turn_On_HighC;
+
+    private void Start()
+    {
+        ActivatedNodePlayer.Play();
+    }
 
     void PlayNodeConnectionSound(int ActivatedNodes, int TotalNodes)
     {
         //Play the low C to start with and the high C at the end
         if (ActivatedNodes == TotalNodes)
         {
-            //Store High C in ActivatedNodePlayer
+            ActivatedNodePlayer.clip = Node_Turn_On_LowC;
         } else if (ActivatedNodes == 1)
         {
-            //Store Low C in ActivatedNodePlayer
+            ActivatedNodePlayer.clip = Node_Turn_On_HighC;
         }
 
         ActivatedNodePlayer.Play();
     }
+
+    //Test Playing SFX with left control button
+#if UNITY_EDITOR
+    int ActivatedNodes = 0;
+
+    public void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            ActivatedNodes++;
+            PlayNodeConnectionSound(ActivatedNodes, 1);
+        }
+    }
+#endif
 }
