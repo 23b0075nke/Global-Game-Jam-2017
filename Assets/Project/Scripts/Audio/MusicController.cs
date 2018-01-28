09eca8cefcc2fd066a8b94b2279c0b22139bb58b
Taro Omiya <taro.omiya@omiyagames.com>
2018-01-27 21:33:33 -0500

    Adjusting the music playing stuff.

--- Assets/Project/Scripts/Audio/MusicController.cs
+++ Assets/Project/Scripts/Audio/MusicController.cs
@@ -6,111 +6,27 @@ using UnityEngine.Audio;
 public class MusicController : MonoBehaviour {
 
     //Variable Definitions
-    int ActivatedNodes;
-    public AudioSource ZeroNodes;
-    public AudioSource OneNode;
-	public AudioSource TwoNodes;
-	public AudioSource ThreeNodes;
-	public AudioSource FourNodes;
-	public AudioSource FiveNodes;
-
-    public void Start()
-    {
-        //Start out with everything but the "zero nodes" track muted
-        ZeroNodes.mute = false;
-        OneNode.mute = true;
-        TwoNodes.mute = true;
-        ThreeNodes.mute = true;
-        FourNodes.mute = true;
-        FiveNodes.mute = true;
-    }
-
-    //Functions
-    
-    //Plays from all the audio mixers directly
-    public void PlayMusic(){
-        ZeroNodes.Play ();
-		OneNode.Play ();
-		TwoNodes.Play ();
-		ThreeNodes.Play ();
-		FourNodes.Play ();
-		FiveNodes.Play ();
-	}
-
-	//Stops all the audio mixers directly
-	public void StopMusic(){
-		ZeroNodes.Stop ();
-		OneNode.Stop ();
-		TwoNodes.Stop ();
-		ThreeNodes.Stop ();
-		FourNodes.Stop ();
-		FiveNodes.Stop ();
-	}
+    private int ActivatedNodes = 0;
+    [SerializeField]
+    private float fadeInDuration = 0.5f;
+    [SerializeField]
+    private AudioMixerSnapshot[] snapshots;
 
     void AddMusicLayer()
     {
         ActivatedNodes += 1;
-
-        if (ActivatedNodes == 1)
-        {
-            OneNode.mute = false;
-
-            ZeroNodes.mute = true;
-            TwoNodes.mute = true;
-            ThreeNodes.mute = true;
-            FourNodes.mute = true;
-            FiveNodes.mute = true;
-
-        } else if (ActivatedNodes == 2)
-        {
-            TwoNodes.mute = false;
-
-            ZeroNodes.mute = true;
-            OneNode.mute = true;
-            ThreeNodes.mute = true;
-            FourNodes.mute = true;
-            FiveNodes.mute = true;
-
-        } else if (ActivatedNodes == 3)
-        {
-            ThreeNodes.mute = false;
-
-            ZeroNodes.mute = true;
-            OneNode.mute = true;
-            TwoNodes.mute = true;
-            FourNodes.mute = true;
-            FiveNodes.mute = true;
-
-        } else if (ActivatedNodes == 4)
-        {
-            FourNodes.mute = false;
-
-            ZeroNodes.mute = true;
-            OneNode.mute = true;
-            TwoNodes.mute = true;
-            ThreeNodes.mute = true;
-            FiveNodes.mute = true;
-
-        } else if (ActivatedNodes == 5)
-        {
-            FiveNodes.mute = false;
-
-            ZeroNodes.mute = true;
-            OneNode.mute = true;
-            TwoNodes.mute = true;
-            ThreeNodes.mute = true;
-            FourNodes.mute = true;
-        }
+        int numNodes = Mathf.Clamp(ActivatedNodes, 0, snapshots.Length);
+        snapshots[numNodes].TransitionTo(fadeInDuration);
     }
 
-
     //Test adding layers by pressing the left control button for now
+#if UNITY_EDITOR
     public void Update()
     {
         if (Input.GetButtonDown("Fire1"))
         {
             AddMusicLayer();
-            ZeroNodes.Stop();
         }
     }
+#endif
 }
