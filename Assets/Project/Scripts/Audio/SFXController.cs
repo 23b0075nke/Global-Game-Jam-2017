a8092bfaf8c63b4c74f4f35580cba3e4bb35f3d4
twainwright <twainwright42@gmail.com>
2018-01-28 10:56:27 -0500

    A little work on polishing SFX and setting up code to make them play

---
+++ Assets/Project/Scripts/Audio/SFXController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFXController : MonoBehaviour {
+
+    [SerializeField]
+    private AudioSource ActivatedNodePlayer;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void PlayNodeConnectionSound(int ActivatedNodes, int TotalNodes)
+    {
+        //Play the low C to start with and the high C at the end
+        if (ActivatedNodes == TotalNodes)
+        {
+            //Store High C in ActivatedNodePlayer
+        } else if (ActivatedNodes == 1)
+        {
+            //Store Low C in ActivatedNodePlayer
+        }
+
+        ActivatedNodePlayer.Play();
+    }
+}
